7971cb4b56d1b12be3c65016f6fac0da374a6eb3
bond187 <bbond2980@gmail.com>
2025-01-21 23:25:30 -0500

    correct answer for day1 puzzle 1

---
+++ day1/day1.py
@@ -0,0 +1,22 @@
+with open('input.txt') as f:
+    lines = f.readlines()
+
+first_list = list()
+second_list = list()
+count = 0
+
+for line in lines:
+    first_list.append(line[0:6])
+    second_list.append(line[7:13])
+
+ordered_first = sorted(first_list)
+ordered_second = sorted(second_list)
+
+total_diff = 0
+
+for f,s in zip(ordered_first, ordered_second):
+    total_diff = total_diff + abs(int(f)-int(s))
+
+print(total_diff)
+               
+            
